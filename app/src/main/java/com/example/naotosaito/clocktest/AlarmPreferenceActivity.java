ba5640697aa3cdf5cbaa93b52c05965db7d8f78b
nsaito <snaoto22@gmail.com>
2018-06-10 22:48:45 +0900

    refs #4 アラーム設定編集機能の追加
    * 時間を設定する項目を追加

--- app/src/main/java/com/example/naotosaito/clocktest/AlarmPreferenceActivity.java
+++ app/src/main/java/com/example/naotosaito/clocktest/AlarmPreferenceActivity.java
@@ -1,18 +1,33 @@
 package com.example.naotosaito.clocktest;
 
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * アラーム設定画面表示用のActivity
  */
 
 public class AlarmPreferenceActivity extends PreferenceActivity {
+    private static final String TAG = "AlarmPreferenceActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new AlarmPreferenceFragment()).commit();
+
+        Log.d(TAG, "onCreate");
+
+        // ボタンの押したときの動作
+        Preference button = findPreference(getString(R.string.alarmtime_key));
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
+            @Override
+            public boolean onPreferenceClick(Preference pref) {
+                Log.d(TAG, "onCreate#onPreferencelick");
+                return true;
+            }
+        });
     }
 }
