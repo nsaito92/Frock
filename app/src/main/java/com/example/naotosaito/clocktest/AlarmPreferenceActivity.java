d31ce92fa3c8fc371cffadd42d255025ce07e842
nsaito <snaoto22@gmail.com>
2018-06-26 02:28:59 +0900

    refs #4 アラーム設定編集機能の追加
    * findpreferenceでnullとなる問題に対応
    * executePendingTransactions()を実行して、保留されてしまったAlarmPreferenceFragment#onCreateが実行される様にした
    * APIレベルによって、findPreferenceをコールするクラスを変更する様にした

--- app/src/main/java/com/example/naotosaito/clocktest/AlarmPreferenceActivity.java
+++ app/src/main/java/com/example/naotosaito/clocktest/AlarmPreferenceActivity.java
@@ -1,5 +1,6 @@
 package com.example.naotosaito.clocktest;
 
+import android.os.Build;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
@@ -11,16 +12,31 @@ import android.util.Log;
 
 public class AlarmPreferenceActivity extends PreferenceActivity {
     private static final String TAG = "AlarmPreferenceActivity";
+    AlarmPreferenceFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new AlarmPreferenceFragment()).commit();
 
-        // ボタンの押したときの動作
-        Preference button = findPreference(getString(R.string.alarmtime_key));
+        mFragment = new AlarmPreferenceFragment();
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, mFragment).commit();
+        // メインスレッドで保留中となるため、AlarmPreferenceFragment#onCreateがコールされない。
+        // findpreferenceを実行したいため、executePendingTransactionsを呼び出す。
+        getFragmentManager().executePendingTransactions();
+
+        /* ボタンの押したときの動作
+         * PreferenceActivity#findPreferenceはAPIレベル11以降非推奨となっているため、
+         * APIレベルが11以上の場合、PreferenceFragment#findPreferenceをコールする。
+         */
+        Preference button = null;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            button = mFragment.findPreference("alarmtime_key");
+        } else {
+            button = findPreference(getString(R.string.alarmtime_key));
+        }
+
         button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
             @Override
             public boolean onPreferenceClick(Preference pref) {
